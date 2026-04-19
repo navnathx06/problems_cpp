b24c79e4f11ba2771d579b6ac13d094df86f2635
Navnath <navnyajade@gmail.com>
2026-04-19 22:27:24 +0530

    Area of circle

---
+++ areaofcircle.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    float r;
+    cout << "Enter Radius : ";  //enter radius
+    
+    cin >> r;
+
+    float area = 3.14 * r * r ;   
+
+    cout<<"area = "<< area << endl;
+
+    return 0;
+}
