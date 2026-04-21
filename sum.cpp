8d453a7e0cfc339802b550e09e7571d42b9ab68e
Navnath <navnyajade@gmail.com>
2026-04-21 23:08:21 +0530

    add two numbers

---
+++ sum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a,b;
+    cout<<"a: ";
+    cin>>a;
+    cout<<"b: ";
+    cin>>b;
+
+    cout<<"sum = "<<a+b;
+
+    return 0;
+}
